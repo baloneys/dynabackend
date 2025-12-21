97874af2cb2173f9b7333620218509f174b3387b
baloneys <157658745+baloneys@users.noreply.github.com>
2025-12-21 17:12:08 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
-import './style.css'
-import Home from './views/home'
-import VRCDynamicPlayerTags from './views/vrc-dynamic-player-tags'
-import NotFound from './views/not-found'
+import './style.css';
+import Home from './views/home';
+import VRCDynamicPlayerTags from './views/vrc-dynamic-player-tags';
+import NotFound from './views/not-found';
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route component={Home} exact path="/" />
-        <Route
-          component={VRCDynamicPlayerTags}
-          exact
-          path="/vrc-dynamic-player-tags"
-        />
-        <Route component={NotFound} path="**" />
-        <Redirect to="**" />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/vrc-dynamic-player-tags" component={VRCDynamicPlayerTags} />
+        <Route path="*" component={NotFound} />
+        <Redirect to="*" />
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
